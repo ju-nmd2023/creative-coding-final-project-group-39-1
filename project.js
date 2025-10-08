48893dd317a3e7f00d3853d3d59de0b5a66c4671
linnmoberg <linn1.moberg@gmail.com>
2025-10-08 10:49:39 +0200

    starting to build

--- project.js
+++ project.js
@@ -1,8 +1,16 @@
 function setup() {
     createCanvas(800, 500);
     noStroke();
+    background(10, 15, 35);
 }
 
 function draw() {
-    background(10, 15, 35);
+    fill(10, 15, 35, 20);
+    rect(0, 0, width, height); // chatGPT helped me here because I could not see the background, https://chatgpt.com/c/68e6249a-2114-8332-a5a1-32b0c14cffdd
+}
+
+class FlowField {
+    constructar (scale) {
+
+    }
 }
